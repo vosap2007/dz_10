911670d08732add51f3a1ac58b463294243685ee
Volodymyr Udovychenko <vosap2007@i.ua>
2020-10-25 11:23:53 +0200

    доделывет всю дз

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+import cardsMenu from './cards.hbs'
+import e from './allCards.json'
 import './styles.css';
+
+const galleryRef = document.querySelector('.js-menu');
+const body = document.querySelector('body');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+
+const markup = cardsMenu(e);
+galleryRef.insertAdjacentHTML('beforeend', markup)
+
+body.addEventListener('change', switchTheme);
+
+function switchTheme () {
+  body.classList.toggle(Theme.LIGHT, !body.classList.toggle(Theme.DARK));
+  localStorage.setItem('Theme', body.classList[0]);
+};
+
+const themeToggle = document.querySelector('#theme-switch-toggle');
+const themeChosen = localStorage.getItem('Theme');
+themeToggle.checked = themeChosen == 'dark-theme' ? true : false;
+body.classList.add(themeChosen ? themeChosen : Theme.LIGHT);
